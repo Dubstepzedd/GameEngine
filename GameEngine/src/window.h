70994ae8c19838a671183b21926b8b4cea3aeee3
Liam Andersson <liaan514@student.liu.se>
2023-07-08 03:45:53 +0200

    Reformed structure in project

---
+++ GameEngine/src/window.h
@@ -0,0 +1,57 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/vec2.hpp>
+
+struct Color {
+	float r;
+	float g;
+	float b;
+	float a;
+};
+
+class Window {
+
+public:
+
+	static Window& getInstance() {
+		static Window window;
+
+		return window;
+	}
+
+	Window(const Window&) = delete;
+	void operator=(const Window&) = delete;
+	int start(const std::string name, const int w, const int h,  bool vSync, bool isResizable);
+	void update();
+	void setSize(const int width, const int height);
+	bool isRunning();
+	void setClearColor(const float r, const float g, const float b, const float a);
+	glm::dvec2 getSize();
+	void makeContext();
+	void destroy();
+	void setVSync(const bool vSync);
+	int getRefreshRate();
+	int getResolutionY();
+	int getResolutionX();
+	void setFullscreen(const bool value);
+	bool isFullscreen();
+	bool isVSync();
+
+	
+private:
+	Window() {};
+	glm::ivec2 wSize{};
+	glm::ivec2 pos{};
+	GLFWwindow* window = nullptr;
+	bool vSync = false;
+	const  GLFWvidmode* videoMode = nullptr;
+	Color clearColor{};
+	bool fullscreen = false;
+
+};
+
+
+#endif
